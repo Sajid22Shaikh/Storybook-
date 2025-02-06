8ef1616b2ef5561fbf958f6317e8510cccaa90a7
Msajid <msajidmuktatrshaikh@gmail.com>
2025-02-06 17:04:10 +0530

    evening update 6

--- src/justdial/justdial.jsx
+++ src/justdial/justdial.jsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import './justdial.css'
 export const Justdial=()=>{
+    const [details,setdetails]=useState([
+        {
+            name:'sajid'
+        },
+        {
+            name:'sajid'
+        },
+        {
+            name:'sajid'
+        },
+        {
+            // name:'sajid'
+        },
+    ])
+
+    const[features,setfeatures]=useState([
+        {},{},{},{},{},{},{},{},{},{},{},{},{},{}
+    ])
+
+    const [services,setservices]=useState([
+        {},{}
+    ])
     return(
         <>
         <div className="container1">
             <div className="head"></div>
             <div className="main">
-                <div className="features"></div>
-                <div className="details"></div>
+                <div className="features">
+                    {features.map(item=>(
+                        <div className="feat"></div>
+                    ))}
+                </div>
+                <div className="details">
+                {details.map(item=>(
+                    <div className="cards">
+                        {/* <p>{item.name}</p> */}
+                    </div>
+                ))}
+                </div>
+                <div className="services">
+                    {services.map(item=>(
+                        <div className="service-cards"></div>
+                    ))}
+                </div>
 
             </div>
             
