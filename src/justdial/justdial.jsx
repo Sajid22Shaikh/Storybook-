63757b7c1abd9ad762dabb8e9914e17f842f2452
Msajid <msajidmuktatrshaikh@gmail.com>
2025-02-06 15:41:01 +0530

    evening update 6

---
+++ src/justdial/justdial.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './justdial.css'
+export const Justdial=()=>{
+    return(
+        <>
+        <div className="container1">
+            <div className="head"></div>
+            <div className="main">
+                <div className="features"></div>
+                <div className="details"></div>
+
+            </div>
+            
+        </div>
+        </>
+    )
+}
---
+++ src/justdial/justdial.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Justdial } from "./justdial";
+
+export default{
+    title:"Design3/Justdial",
+    component:Justdial
+}
+
+export const Default=()=><Justdial/>
