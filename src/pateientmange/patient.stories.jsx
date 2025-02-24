b94ce7a1d5ea194340a63c5c36e396db38fe9cac
Msajid <msajidmuktatrshaikh@gmail.com>
2025-02-24 16:08:19 +0530

    latest24

---
+++ src/pateientmange/patient.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Patient } from "./patient";
+
+export default{
+    title:'PatientManegement/patient',
+    component:Patient,
+}
+
+export const Default=()=><Patient/>
