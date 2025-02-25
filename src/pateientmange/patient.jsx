493304df204ba394c43cd92c11d1b9f67cff8195
Msajid <msajidmuktatrshaikh@gmail.com>
2025-02-25 16:49:04 +0530

    latest25

--- src/pateientmange/patient.jsx
+++ src/pateientmange/patient.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Search } from 'lucide-react';
 
+import './patient.css'
 export const Patient=()=>{
     return(
         <>
-        <div>sajid</div>
+        <div className="pcontainer">
+            <div className="pheader"></div> 
+            <div className="pcontent">
+                <div className="psidebar"></div>
+                <div className="pmain">
+                    <div className="pque">
+                        <div className="psearch">
+                            <h2>Patient Queue</h2>
+                            <Search/>
+                        </div>
+                        <div className="pcatogories"></div>
+                        <div className="plist"></div>
+                    </div>
+                    <div className="pdetail"></div>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
